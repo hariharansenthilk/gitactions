67afebddf759795f201ec8a8cd61425403166f8b
hariharansenthilk <hariharansenthilkumar32@gmail.com>
2026-05-22 11:51:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get('/api', (req, res) => {
-    res.json({ message: "Hello from Backend 🚀" });
+    res.json({ message: "look at me... 🚀" });
 });
 
 app.listen(PORT, () => {
